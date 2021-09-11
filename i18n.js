a3dfd18daad5b55d6015069e45d62c3c6323a382
Edgar Butwilowski <postfach@mail.ch>
2021-09-11 15:42:45 +0200

    Minor bugfix in localization code.

--- i18n.js
+++ i18n.js
@@ -2,7 +2,7 @@
 let lang = navigator.language || navigator.userLanguage;
 if(lang.startsWith("de")) {
     document.getElementById("i18n-title").innerHTML = "Einen &uuml;berf&uuml;llten M&uuml;lleimer melden";
-    document.getElementById("i18n_bin_is_full").innerHTML = "Hallo, der M&uuml;lleimer ist &uuml;berf&uuml;llt? Sie m&ouml;chten das an den Betreiber melden?";
-    document.getElementById("i18n_right_place").innerHTML = "Dann sind Sie hier genau richtig!";
-    document.getElementById("i18n_opens_mail").innerHTML = "Der folgende Knopf &ouml;ffnet eine vorgefertigte E-Mail in Ihrer E-Mail-App. Sie m&uuml;ssen dann nur noch die E-Mail absenden, um die &Uuml;berf&uuml;llung zu melden.";
+    document.getElementById("i18n-bin-is-full").innerHTML = "Hallo, der M&uuml;lleimer ist &uuml;berf&uuml;llt? Sie m&ouml;chten das an den Betreiber melden?";
+    document.getElementById("i18n-right-place").innerHTML = "Dann sind Sie hier genau richtig!";
+    document.getElementById("i18n-opens-mail").innerHTML = "Der folgende Knopf &ouml;ffnet eine vorgefertigte E-Mail in Ihrer E-Mail-App. Sie m&uuml;ssen dann nur noch die E-Mail absenden, um die &Uuml;berf&uuml;llung zu melden.";
 }
